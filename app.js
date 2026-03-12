147cd810552959ebfe07e58ca26a998ae61cae84
Nandini-tl <nandini@tilicho.in>
2026-03-12 22:56:31 +0530

    Add Operation

---
+++ app.js
@@ -0,0 +1,11 @@
+// calculator-app/app.js - MODULE 1
+function calculate(num1, num2, operation) {
+    console.log(`Calculating ${num1} ${operation} ${num2}`);
+ 
+    if (operation === "add") {
+        return num1 + num2; 
+    }
+ 
+    return "Operation not implemented";
+}
+console.log(calculate(8, 3, "add")); 
